aa83baa4b3b799894a4d10886de20ca23c136ee1
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-02-04 14:55:43 +0530

    complete app done

---
+++ index.js
@@ -0,0 +1,6 @@
+function genrateCat() {
+    let image = document.createElement('img');
+    let div = document.getElementById('flex-cat-genrator');
+    image.src = "https://api.thecatapi.com/v1/images/search?mime_types=gif&size =small&format=src";
+    div.appendChild(image);
+}
