9b66d06d013da6b18cd798e6e59d05c9916124d8
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-01-11 03:30:55 +0530

    Updated functions to ES6 arrow functions 🎃

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function genrateCat() {
+const genrateCat = () => {
     let image = document.createElement('img');
     let div = document.getElementById('flex-cat-genrator');
     image.src = "https://api.thecatapi.com/v1/images/search?mime_types=gif&size =small&format=src";
